548ea4f3f68178a17ef9450601fb110b3caeb013
David Meir-Levy <davidmeirlevy@gmail.com>
2022-03-08 22:09:44 +0200

    stage 8: add product to cart

--- components/compositions/cart.js
+++ components/compositions/cart.js
@@ -4,5 +4,21 @@ export function useCart() {
 	const checkoutStatus = ref(null);
 	const items = ref([]);
 
-	return {}
+	function addProductToCart(product) {
+		checkoutStatus.value = null;
+		if (product.inventory > 0) {
+			const cartItem = items.value.find(item => item.id === product.id);
+			if (!cartItem) {
+				items.value.push({
+					id,
+					quantity: 1
+				});
+			} else {
+				cartItem.quantity++;
+			}
+		}
+		// TODO: call decrementProductInventory
+	}
+
+	return { addProductToCart }
 }

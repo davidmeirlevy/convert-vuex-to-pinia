b8909e08307c122c87d0c4632426d72f1df0677a
David Meir-Levy <davidmeirlevy@gmail.com>
2022-03-08 21:47:27 +0200

    stage 3: install pinia

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue'
 import App from './components/App.vue'
 import store from './store'
+import { createPinia } from 'pinia'   // <-------- import pinia
+
 
 const app = createApp(App)
 
 app.use(store)
+app.use(createPinia())                // <-------- use it as plugin
 
 app.mount('#app')

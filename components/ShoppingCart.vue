b194a358fb259be0c6f10610bea1a647809ea505
David Meir-Levy <davidmeirlevy@gmail.com>
2022-03-08 22:31:07 +0200

    stage 11: use cart composition in cart component

--- components/ShoppingCart.vue
+++ components/ShoppingCart.vue
@@ -1,42 +1,42 @@
 <template>
-  <div class="cart">
-    <h2>Your Cart</h2>
-    <p v-show="!products.length">
-      <i>Please add some products to cart.</i>
-    </p>
-    <ul>
-      <li v-for="product in products" :key="product.id">
-        {{ product.title }} - {{ currency(product.price) }} x {{ product.quantity }}
-      </li>
-    </ul>
-    <p>Total: {{ currency(total) }}</p>
-    <p><button :disabled="!products.length" @click="checkout(products)">Checkout</button></p>
-    <p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
-  </div>
+	<div class="cart">
+		<h2>Your Cart</h2>
+		<p v-show="!products.length">
+			<i>Please add some products to cart.</i>
+		</p>
+		<ul>
+			<li v-for="product in products" :key="product.id">
+				{{ product.title }} - {{ currency(product.price) }} x {{ product.quantity }}
+			</li>
+		</ul>
+		<p>Total: {{ currency(total) }}</p>
+		<p>
+			<button :disabled="!products.length" @click="checkout(products)">Checkout</button>
+		</p>
+		<p v-show="checkoutStatus">Checkout {{ checkoutStatus }}.</p>
+	</div>
 </template>
 
 <script>
-import { computed } from 'vue'
 import { useStore } from 'vuex'
 import { currency } from '../currency'
+import { useCart } from './compositions/cart';
 
 export default {
-  setup () {
-    const store = useStore()
+	setup() {
+		const store = useStore()
 
-    const checkoutStatus = computed(() => store.state.cart.checkoutStatus)
-    const products = computed(() => store.getters['cart/cartProducts'])
-    const total = computed(() => store.getters['cart/cartTotalPrice'])
+		const { checkoutStatus, cartProducts, cartTotalPrice } = useCart()
 
-    const checkout = (products) => store.dispatch('cart/checkout', products)
+		const checkout = (products) => store.dispatch('cart/checkout', products)
 
-    return {
-      currency,
-      checkoutStatus,
-      products,
-      total,
-      checkout
-    }
-  }
+		return {
+			currency,
+			checkoutStatus,
+			products: cartProducts,
+			total: cartTotalPrice,
+			checkout
+		}
+	}
 }
 </script>
